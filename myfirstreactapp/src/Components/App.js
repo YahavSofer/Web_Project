365a45152163977d65144658bae1a02ab03c9657
YahavSofer <yahavss93@gmail.com>
2021-10-27 23:26:51 +0300

    first step of authentication

--- myfirstreactapp/src/Components/App.js
+++ myfirstreactapp/src/Components/App.js
@@ -1,15 +1,19 @@
-import React, { Component }  from "react";
+import React  from "react";
 import Signup from "./Signup";
 import {Container} from 'react-bootstrap'
+import { AuthProvider } from "../context/AuthContext";
+
+// require('dotenv').config();
 
 function App() {
   return (
-    <Container className = 'd-flex align-items-center justify-content-center' style={{minHeight:"100vh"}}>
-      <div className='w-100' style={{maxWidth:"360px"}}>
-        <Signup />
-      </div>
-    </Container>
-
+    <AuthProvider>
+        <Container className = 'd-flex align-items-center justify-content-center' style={{minHeight:"100vh"}}>
+          <div className='w-100' style={{maxWidth:"360px"}}>
+            <Signup />
+          </div>
+        </Container>
+    </AuthProvider>
   );
 }
 
--- myfirstreactapp/src/Components/Signup.js
+++ myfirstreactapp/src/Components/Signup.js
@@ -1,16 +1,40 @@
-import React, {useRef} from 'react'
-import {Button, Card, Form} from 'react-bootstrap'
+import React, {useRef, useState} from 'react'
+import {Alert, Button, Card, Form} from 'react-bootstrap'
+import { useAuth } from '../context/AuthContext'
 
 export default function Signup() {
     const emailRef = useRef()
     const passwordRef = useRef()
     const passworConfirmationdRef =useRef()
+    const {signup} = useAuth()
+    const [error, setError] = useState("")
+    const [loading, setLoading] = useState(false)
+
+
+ async function handleSubmit(e){
+        e.preventDefault()
+
+        if(passwordRef.current.value !== passworConfirmationdRef.current.value){
+                return setError('Passwords do not match')
+        }
+
+        try{
+            setError("")
+            setLoading(true)
+            await signup(emailRef.current.value , passwordRef.current.value)
+        }catch{
+            setError('Failed to create an account')
+        }
+        setLoading(false)
+        
+    }
     return (
         <>
         <Card>
             <Card.Body>
                 <h2 className="text-center mb-4">Sign Up</h2>
-                <Form>
+                {error && <Alert variant="danger">{error}</Alert>}
+                <Form onSubmit = {handleSubmit}>
                     <Form.Group id="email">
                         <Form.Label>Email</Form.Label>
                         <Form.Control type="email" ref={emailRef} required/> 
@@ -23,7 +47,11 @@ export default function Signup() {
                         <Form.Label>Password Confirmation</Form.Label>
                         <Form.Control type='password' ref={passworConfirmationdRef} required/> 
                     </Form.Group>
-                    <Button type='submit' className='w-100'>Sign up</Button>        
+                    
+                    <Button disabled={loading} type='submit' className='w-100'>
+                        Sign up
+                    </Button>
+
                 </Form>               
             </Card.Body>
         </Card>
