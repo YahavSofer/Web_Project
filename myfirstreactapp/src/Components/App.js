76a72bad9add06708e93bcbfe8d3da37de137a8e
YahavSofer <yahavss93@gmail.com>
2021-10-27 23:45:26 +0300

    create a route to Signup

--- myfirstreactapp/src/Components/App.js
+++ myfirstreactapp/src/Components/App.js
@@ -2,18 +2,29 @@ import React  from "react";
 import Signup from "./Signup";
 import {Container} from 'react-bootstrap'
 import { AuthProvider } from "../context/AuthContext";
+import {BrowserRouter as Router, Switch ,Route} from 'react-router-dom'
 
-// require('dotenv').config();
+
 
 function App() {
   return (
-    <AuthProvider>
+    
         <Container className = 'd-flex align-items-center justify-content-center' style={{minHeight:"100vh"}}>
           <div className='w-100' style={{maxWidth:"360px"}}>
-            <Signup />
+          
+                  <Router>
+                      <AuthProvider>
+                        <Switch>
+                            <Route path="/signup" component={Signup}/>
+                        </Switch>
+                      </AuthProvider>
+                  </Router>
+
+                  <Signup />
           </div>
+          
         </Container>
-    </AuthProvider>
+    
   );
 }
 
